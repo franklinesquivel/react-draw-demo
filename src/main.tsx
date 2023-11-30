261642e5fb322530a986262a0e70b122678f50cc
Franklin Esquivel <franklin@ese.plus>
2023-11-29 23:14:30 -0600

    style(router): Adds react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import App      from '@/App';
-import React    from 'react';
-import ReactDOM from 'react-dom/client';
+import { router }         from '@/router';
+import React              from 'react';
+import ReactDOM           from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={ router } />
     </React.StrictMode>
 );
