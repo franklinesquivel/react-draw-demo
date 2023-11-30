00548fbf5641716dba5d7f364ecfceb63ee206c9
Franklin Esquivel <franklin@ese.plus>
2023-11-30 00:09:46 -0600

    feat(styles): Defines main app styles file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { router }         from '@/router';
 import React              from 'react';
 import ReactDOM           from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import './main.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
